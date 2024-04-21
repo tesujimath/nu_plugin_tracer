06b3c099285636b128a315d0d18097175fba0409
Simon Guest <simon.guest@tesujimath.org>
2024-04-22 11:07:43 +1200

    First working version, but doesn't exit when plugin is done

---
+++ src/main.rs
@@ -0,0 +1,124 @@
+use anyhow::{anyhow, Result};
+use home::home_dir;
+use std::{
+    env::args_os,
+    path::{Path, PathBuf},
+    pin::Pin,
+    process::Stdio,
+};
+use tokio::{
+    fs::OpenOptions,
+    io::{stdin, stdout, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
+    pin,
+    process::Command,
+};
+
+fn program_name() -> Result<String> {
+    args_os()
+        .next()
+        .ok_or(anyhow!("can't find own program name"))
+        .and_then(|s| {
+            Ok(Path::new(&s)
+                .file_name()
+                .ok_or(anyhow!("failed to determine file_name for program"))?
+                .to_str()
+                .ok_or(anyhow!("program name is not valid unicode"))?
+                .to_string())
+        })
+}
+
+fn default_plugins_dir() -> Result<PathBuf> {
+    let config_dir: PathBuf = nu_path::config_dir().ok_or(anyhow!("can't find Nu config dir"))?;
+
+    Ok(config_dir.join("nushell").join("plugins"))
+}
+
+async fn forward<R, W, Tee>(
+    label: &str,
+    mut r: Pin<&mut R>,
+    mut w: Pin<&mut W>,
+    mut tee: Pin<&mut Tee>,
+) -> std::io::Result<()>
+where
+    R: AsyncRead,
+    W: AsyncWrite,
+    Tee: AsyncWrite,
+{
+    let mut done = false;
+    let mut buf: [u8; 1024] = [0; 1024];
+
+    while !done {
+        tracing::info!("{} attempt read", label);
+        let n_read = r.read(&mut buf).await?;
+        if n_read == 0 {
+            done = true;
+        } else {
+            w.write_all(&buf[..n_read]).await?;
+            w.flush().await?;
+            tee.write_all(&buf[..n_read]).await?;
+            tee.flush().await?;
+        }
+        tracing::info!("{} forward {} bytes", label, n_read);
+    }
+    tracing::info!("{} done", label);
+
+    Ok(())
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> anyhow::Result<()> {
+    let home = home_dir().ok_or(anyhow!("can't determine user home directory"))?;
+    let appender = tracing_appender::rolling::never(home, "nu_plugin_tracer.log");
+    let (non_blocking_appender, _guard) = tracing_appender::non_blocking(appender);
+    let subscriber = tracing_subscriber::fmt().with_writer(non_blocking_appender);
+    tracing::subscriber::set_global_default(subscriber.finish())?;
+
+    let tracer_name = program_name()?;
+    let suffix = "_tracer";
+
+    let plugin_name = tracer_name
+        .strip_suffix("_tracer")
+        .ok_or(anyhow!("program name doesn't end with {}", suffix))?;
+    let plugin_path = default_plugins_dir().map(|p| p.join(plugin_name))?;
+
+    let mut plugin = Command::new(&plugin_path)
+        .args(std::env::args().skip(1))
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+    let (plugin_stdin, plugin_stdout) =
+        (plugin.stdin.take().unwrap(), plugin.stdout.take().unwrap());
+
+    tracing::info!("tracing {:?}", &plugin_path);
+
+    let raw_in_path = PathBuf::from(format!("{}.in.raw", &plugin_name));
+    let raw_in = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(&raw_in_path)
+        .await?;
+    pin!(raw_in);
+    let raw_out_path = PathBuf::from(format!("{}.out.raw", &plugin_name));
+    let raw_out = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(&raw_out_path)
+        .await?;
+    pin!(raw_out);
+
+    let stdin = stdin();
+    let stdout = stdout();
+    pin!(stdin);
+    pin!(stdout);
+    pin!(plugin_stdin);
+    pin!(plugin_stdout);
+    let res = tokio::try_join!(
+        forward("in", stdin, plugin_stdin, raw_in),
+        forward("out", plugin_stdout, stdout, raw_out),
+        plugin.wait()
+    );
+
+    tracing::info!("tracer is done");
+
+    res.map(|_| ()).map_err(|e| e.into())
+}
